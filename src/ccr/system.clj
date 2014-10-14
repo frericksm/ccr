39bed4e6b60927c592b80bf32e6e27db02fa2d45
Michael Frericks <Michael-Frericks@web.de>
2014-10-14 23:13:51 +0200

    initial

--- src/ccr/system.clj
+++ src/ccr/system.clj
@@ -6,10 +6,10 @@
   "Returns a new instance of the whole application."
   []
   {:uri "datomic:mem://dev"
-   :import-file "C:/1111/Rheinlandmobil/rheinland-mobil-1.0.3-2.xml"
+   :import-file "content.xml"
    :tx-out-file "c:/1111/tx-data.txt"
-   :builtin-nodetypes-file "C:/Projekte/isp-head/ContentEditor/Prod/META-INF/repository/builtin.xml_"
-   :custom-nodetypes-file "C:/jcr111/repository/nodetypes/custom_nodetypes.xml"
+   :builtin-nodetypes-file "???/builtin.xml_"
+   :custom-nodetypes-file "???custom_nodetypes.xml"
    })
 
 
