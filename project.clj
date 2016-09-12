cb38e1169a08d8471855307571cc51eeceb965d8
Michael Frericks <Michael-Frericks@web.de>
2016-09-12 16:51:52 +0200

    upgrade clojure and instaparse version

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
 (defproject ccr "0.1.0-SNAPSHOT"
   :description "A clojure content repository system"
-  :dependencies [[org.clojure/clojure "1.7.0-alpha4"]
+  :dependencies [[org.clojure/clojure "1.9.0-alpha12"]
                  ;;[org.clojure/clojure "1.6.0"]
                  [org.clojure/data.zip "0.1.1"]
                  [org.clojure/data.xml "0.0.8"]
-                 [instaparse "1.3.5"]
+                 [instaparse "1.4.3"]
                  [enlive "1.1.5"]
                  [javax.jcr/jcr "2.0"]
                  [org.clojure/data.codec "0.1.0"]
@@ -13,7 +13,7 @@
   :repositories {"my.datomic.com" {:url "https://my.datomic.com/repo"
                                    :creds :gpg}}
   :profiles {:dev {:dependencies [[org.clojure/tools.namespace "0.2.4"]
-                                  [com.datomic/datomic-free "0.9.5078"
+                                  [com.datomic/datomic-free "0.9.5394"
                                    :exclusions [joda-time]]]
                    :source-paths ["dev"]}
              :db2 {:dependencies
