e18d1f3f0e262656ca8f290e0cc39753e9fa1302
Michael Frericks <michael-frericks@web.de>
2017-11-05 00:56:25 +0100

    import cmd

--- dev/play.clj
+++ dev/play.clj
@@ -22,7 +22,7 @@
 
 
 
-(import/import-xml s rn "/home/michael/projects/ccr/samples/c2.xml")
+(import/import-xml s rn "/home/michael/projects/ccr/samples/content.xml")
 
 (as-> (n/nodes (n/node rn "V1.0.3/R/Auslaufende Preisbefreiung/Auslaufende Preisbefreiung/isp:activities/KONTO_LESEN_2/isp:activityBody/isp:dataMappings/isp:dataMapping[2]")) x
         (map n/item-name x))
